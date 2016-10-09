fbed270ae72a3bd0f45e2ec642f68620ce4715c9
odins41 <odins41@mail.ru>
2016-10-09 14:37:20 +0300

    Update pprint_json.py

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,19 @@
 import json
 
 
-def load_data(filepath):
-    pass
+def load_data(data):
+    with open(data) as json_file:
+        json_string = json_file.read()
+        parsed_string = json.loads(json_string)
+        return parsed_string
 
 
 def pretty_print_json(data):
-    pass
+    pretty_print = (json.dumps(parsed_string, sort_keys=True, indent=4, ensure_ascii=False))
+    return pretty_print 
 
 
 if __name__ == '__main__':
-    pass
+    data = input("введите путь к файлу\n")
+    parsed_string = load_data(data)
+    print (pretty_print_json(parsed_string))
